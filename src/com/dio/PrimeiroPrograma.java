5df0216ea074cd3ec15e6c543814364c9e7d9b09
João <joaocassioab1@gmail.com>
2022-05-11 18:44:32 -0300

    First

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();	
+			
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		}
+
+}
+
+		class Livro {
+			private String nome;
+			private String npag;
+		}
